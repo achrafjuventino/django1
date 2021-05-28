c350b206e95eb6997ea2c089308d6d40b4869dab
achrafjuventino <60466087+achrafjuventino@users.noreply.github.com>
2021-05-28 23:17:43 +0100

    Add files via upload

---
+++ Client.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atelier_2
+{
+    class Client
+    {
+        private string Nom;
+        private string Prénom;
+        private string Adresse;
+
+        public Client(string n, string p, string a)
+        {
+            this.Nom = n;
+            this.Prénom = p;
+            this.Adresse = a;
+        }
+        public void Afficher()
+        {
+            Console.WriteLine("Le nom est : " + this.Nom);
+            Console.WriteLine("Le prénom est : " + this.Prénom);
+            Console.WriteLine("L'adress est : " + this.Adresse);
+        }
+
+    }
+}
